083d26da034b8225b07ca183eb2b534c02ad52eb
harald <harald@free-creations.de>
2025-08-13 17:56:57 +0200

    simplify plugin structure and replace FloatParam with IntParam for gain control

--- examples/tiny_gain_plug/src/lib.rs
+++ examples/tiny_gain_plug/src/lib.rs
@@ -1,59 +1,23 @@
 use nih_plug::prelude::*;
-use parking_lot::Mutex;
 use std::sync::Arc;
 
 struct TinyGainPlug {
     params: Arc<TinyGainParams>,
+    previous_gain: i32,
 }
-
-/// The [`Params`] derive macro gathers all of the information needed for the wrapper to know about
-/// the plugin's parameters, persistent serializable fields, and nested parameter groups. You can
-/// also easily implement [`Params`] by hand if you want to, for instance, have multiple instances
-/// of a parameters struct for multiple identical oscillators/filters/envelopes.
 #[derive(Params)]
 struct TinyGainParams {
-    /// The parameter's ID is used to identify the parameter in the wrapped plugin API. As long as
-    /// these IDs remain constant, you can rename and reorder these fields as you wish. The
-    /// parameters are exposed to the host in the same order they were defined. In this case, this
-    /// gain parameter is stored as linear gain while the values are displayed in decibels.
+    /// We make the gain parameter an integer between -100 and 20 dB.
     #[id = "gain"]
-    pub gain: FloatParam,
-
-    /// This field isn't used in this example, but anything written to the vector would be restored
-    /// together with a preset/state file saved for this plugin. This can be useful for storing
-    /// things like sample data.
-    #[persist = "industry_secrets"]
-    pub random_data: Mutex<Vec<f32>>,
-
-    /// You can also nest parameter structs. These will appear as a separate nested group if your
-    /// DAW displays parameters in a tree structure.
-    #[nested(group = "Subparameters")]
-    pub sub_params: SubParams,
-
-    /// Nested parameters also support some advanced functionality for reusing the same parameter
-    /// struct multiple times.
-    #[nested(array, group = "Array Parameters")]
-    pub array_params: [ArrayParams; 3],
+    pub gain: IntParam,
 }
 
-#[derive(Params)]
-struct SubParams {
-    #[id = "thing"]
-    pub nested_parameter: FloatParam,
-}
-
-#[derive(Params)]
-struct ArrayParams {
-    /// This parameter's ID will get a `_1`, `_2`, and a `_3` suffix because of how it's used in
-    /// `array_params` above.
-    #[id = "noope"]
-    pub nope: FloatParam,
-}
 
 impl Default for TinyGainPlug {
     fn default() -> Self {
         Self {
             params: Arc::new(TinyGainParams::default()),
+            previous_gain: 0,
         }
     }
 }
@@ -61,61 +25,20 @@ impl Default for TinyGainPlug {
 impl Default for TinyGainParams {
     fn default() -> Self {
         Self {
-            // This gain is stored as linear gain. NIH-plug comes with useful conversion functions
-            // to treat these kinds of parameters as if we were dealing with decibels. Storing this
-            // as decibels is easier to work with, but requires a conversion for every sample.
-            gain: FloatParam::new(
+            gain: IntParam::new(
                 "Gain",
-                util::db_to_gain(0.0),
-                FloatRange::Skewed {
-                    min: util::db_to_gain(-30.0),
-                    max: util::db_to_gain(30.0),
-                    // This makes the range appear as if it was linear when displaying the values as
-                    // decibels
-                    factor: FloatRange::gain_skew_factor(-30.0, 30.0),
-                },
-            )
-                // Because the gain parameter is stored as linear gain instead of storing the value as
-                // decibels, we need logarithmic smoothing
-                .with_smoother(SmoothingStyle::Logarithmic(50.0))
-                .with_unit(" dB")
-                // There are many predefined formatters we can use here. If the gain was stored as
-                // decibels instead of as a linear gain value, we could have also used the
-                // `.with_step_size(0.1)` function to get internal rounding.
-                .with_value_to_string(formatters::v2s_f32_gain_to_db(2))
-                .with_string_to_value(formatters::s2v_f32_gain_to_db()),
-            // Persisted fields can be initialized like any other fields, and they'll keep their
-            // values when restoring the plugin's state.
-            random_data: Mutex::new(Vec::new()),
-            sub_params: SubParams {
-                nested_parameter: FloatParam::new(
-                    "Unused Nested Parameter",
-                    0.5,
-                    FloatRange::Skewed {
-                        min: 2.0,
-                        max: 2.4,
-                        factor: FloatRange::skew_factor(2.0),
-                    },
-                )
-                    .with_value_to_string(formatters::v2s_f32_rounded(2)),
-            },
-            array_params: [1, 2, 3].map(|index| ArrayParams {
-                nope: FloatParam::new(
-                    format!("Nope {index}"),
-                    0.5,
-                    FloatRange::Linear { min: 1.0, max: 2.0 },
-                ),
-            }),
+                0,
+                IntRange::Linear {min: -100, max: 20}, )
         }
     }
 }
 
 impl Plugin for TinyGainPlug {
     const NAME: &'static str = "TinyGainPlug";
-    const VENDOR: &'static str = "Moist Plugins GmbH";
+    const VENDOR: &'static str = "Harald-LB";
     // You can use `env!("CARGO_PKG_HOMEPAGE")` to reference the homepage field from the
     // `Cargo.toml` file here
-    const URL: &'static str = "https://youtu.be/dQw4w9WgXcQ";
+    const URL: &'static str = "https://example.com/TinyGainPlug";
     const EMAIL: &'static str = "info@example.com";
 
     const VERSION: &'static str = env!("CARGO_PKG_VERSION");
@@ -141,24 +64,9 @@ impl Plugin for TinyGainPlug {
             ..AudioIOLayout::const_default()
         },
     ];
-
-    const MIDI_INPUT: MidiConfig = MidiConfig::None;
-    // Setting this to `true` will tell the wrapper to split the buffer up into smaller blocks
-    // whenever there are inter-buffer parameter changes. This way no changes to the plugin are
-    // required to support sample accurate automation and the wrapper handles all of the boring
-    // stuff like making sure transport and other timing information stays consistent between the
-    // splits.
     const SAMPLE_ACCURATE_AUTOMATION: bool = true;
-
-    // If the plugin can send or receive SysEx messages, it can define a type to wrap around those
-    // messages here. The type implements the `SysExMessage` trait, which allows conversion to and
-    // from plain byte buffers.
     type SysExMessage = ();
-    // More advanced plugins can use this to run expensive background tasks. See the field's
-    // documentation for more information. `()` means that the plugin does not have any background
-    // tasks.
     type BackgroundTask = ();
-
     fn params(&self) -> Arc<dyn Params> {
         self.params.clone()
     }
@@ -170,36 +78,30 @@ impl Plugin for TinyGainPlug {
 
     fn process(
         &mut self,
-        buffer: &mut Buffer,
+        _buffer: &mut Buffer,
         _aux: &mut AuxiliaryBuffers,
         _context: &mut impl ProcessContext<Self>,
     ) -> ProcessStatus {
-        for channel_samples in buffer.iter_samples() {
-            // Smoothing is optionally built into the parameters themselves
-            let gain = self.params.gain.smoothed.next();
-
-            for sample in channel_samples {
-                *sample *= gain;
-            }
-        }
+        let gain_db = &self.params.gain.value();
+        if gain_db != &self.previous_gain {
+            nih_log!("Gain changed to {}", gain_db);
+            self.previous_gain = *gain_db;
+        }   
+        
+       // for channel_samples in buffer.iter_samples() {  }
 
         ProcessStatus::Normal
     }
-
-    // This can be used for cleaning up special resources like socket connections whenever the
-    // plugin is deactivated. Most plugins won't need to do anything here.
-    fn deactivate(&mut self) {}
 }
 
 impl ClapPlugin for TinyGainPlug {
-    const CLAP_ID: &'static str = "com.moist-plugins-gmbh.gain";
+    const CLAP_ID: &'static str = "com.mystic-plugins-gmbh.gain";
     const CLAP_DESCRIPTION: Option<&'static str> = Some("A smoothed gain parameter example plugin");
     const CLAP_MANUAL_URL: Option<&'static str> = Some(Self::URL);
     const CLAP_SUPPORT_URL: Option<&'static str> = None;
     const CLAP_FEATURES: &'static [ClapFeature] = &[
         ClapFeature::AudioEffect,
         ClapFeature::Stereo,
-        ClapFeature::Mono,
         ClapFeature::Utility,
     ];
 }
